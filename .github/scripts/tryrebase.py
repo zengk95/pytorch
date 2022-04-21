27a9cb17496a9886f25f65bc018664c528ac634d
Catherine Lee <csl@fb.com>
2022-04-21 16:06:34 -0700

    .

--- .github/scripts/tryrebase.py
+++ .github/scripts/tryrebase.py
@@ -10,29 +10,25 @@ def parse_args() -> Any:
     from argparse import ArgumentParser
     parser = ArgumentParser("Rebase PR into branch")
     parser.add_argument("--dry-run", action="store_true")
-    parser.add_argument("--rebase", action="store_true")
-    parser.add_argument("--comment-id", type=int)
     parser.add_argument("--branch", type=str)
     parser.add_argument("pr_num", type=int)
     return parser.parse_args()
 
 
-def rebase_onto(pr: GitHubPR, repo: GitRepo, branch: str, dry_run: bool = False) -> None:
-    pr_branch_name = f"rebase-pr-{pr.pr_num}"
-    repo.fetch(f"pull/{pr.pr_num}/head", pr_branch_name)
-    if branch is None:
-        branch = pr.default_branch()
-    print(repo._run_git("rebase", branch, pr_branch_name))
+def rebase_onto(pr: GitHubPR, repo: GitRepo, onto_branch: str, dry_run: bool = False) -> None:
+    branch = f"pull/{pr.pr_num}/head"
+    repo.fetch(branch, branch)
+    if onto_branch is None:
+        onto_branch = pr.default_branch()
+    repo._run_git("rebase", onto_branch, branch)
     remote = f"https://github.com/{pr.info['headRepository']['nameWithOwner']}.git"
-    refspec = f"{pr_branch_name}:{pr.head_ref()}"
+    refspec = f"{branch}:{pr.head_ref()}"
     if dry_run:
         repo._run_git("push", "--dry-run", "-f", remote, refspec)
     else:
         push_result = repo._run_git("push", "-f", remote, refspec)
-        print(push_result)
         if "Everything up-to-date" in push_result:
-            gh_post_comment(pr.org, pr.project, pr.pr_num, "tried to rebase and push, but was already up to date", dry_run=dry_run)
-
+            gh_post_comment(pr.org, pr.project, pr.pr_num, f"Tried to rebase and push PR #{pr.pr_num}, but it was already up to date", dry_run=dry_run)
 
 
 def main() -> None:
