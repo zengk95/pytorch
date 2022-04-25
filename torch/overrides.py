f8d69042b80f4f9b838b0a57e29d625d1497bbb1
samdow <samdow@fb.com>
2022-04-25 16:25:15 -0400

    update enable_python_mode to match enable_torch_function_mode
    ghstack-source-id: 04ea5d9b993a50a2d37fb3891048c3ed87140438
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75965

--- torch/overrides.py
+++ torch/overrides.py
@@ -34,6 +34,8 @@ from torch._C import (
     _has_torch_function_variadic, _add_docstr, _set_torch_function_mode, _get_torch_function_mode)
 import contextlib
 
+from torch.utils._mode_utils import _enable_mode, _ModeInfo
+
 __all__ = [
     "get_ignored_functions",
     "get_overridable_functions",
@@ -1785,6 +1787,37 @@ def _no_torch_function_mode() -> Iterator[None]:
         _set_torch_function_mode(old)
 
 
+class TorchFunctionModeInfo(_ModeInfo):
+    def __init__(self):
+        super().__init__(mode_name="torch_function", mode_class=TorchFunctionMode,
+                         base_mode_class=BaseTorchFunctionMode,
+                         required_fn="__torch_function__")
+
+    def is_allowed_type(self, mode) -> bool:
+        return mode is None or\
+            isinstance(mode, self.mode_class) or\
+            (isinstance(mode, type) and not issubclass(mode, self.mode_class))
+
+    def allowed_types_for_error_message(self) -> str:
+        return "TorchFunctionMode, Tensor-like class, or None"
+
+    def get_mode(self):
+        return _get_torch_function_mode()
+
+    def help_text(self, mode) -> str:
+        if isinstance(mode, self.mode_class):
+            return f'Use push_{self.mode_type}_mode instead.'
+        else:
+            return (
+                'If you intended to completely override the preexisting mode, '
+                'pass ignore_preexisting=True.  This can result in unexpected '
+                'behavior; please consider rewriting your mode to be a subclass '
+                f'of {self.mode_class.__name__} to make it compositional!'
+            )
+
+    def set_mode(self, mode):
+        return _set_torch_function_mode(mode)
+
 @contextlib.contextmanager
 def enable_torch_function_mode(mode, *, replace=None, ignore_preexisting=False) -> Iterator[None]:
     """
@@ -1815,49 +1848,7 @@ def enable_torch_function_mode(mode, *, replace=None, ignore_preexisting=False) 
         ignore_preexisting (bool): if True, ignore any preexisting mode
             and overwrite it with the passed mode.
     """
-    if not (
-        mode is None or
-        isinstance(mode, TorchFunctionMode) or
-        (isinstance(mode, type) and not issubclass(mode, TorchFunctionMode))
-    ):
-        raise TypeError(
-            "expected to get TorchFunctionMode, Tensor-like class or None as argument, got "
-            f"{type(mode)} instead"
-        )
-    old = _get_torch_function_mode()
-    # Short circuit.  It is valid to reset a mode to yourself, we won't error
-    # here
-    if old is mode:
-        yield
-        return
-    if old is not None and not ignore_preexisting and old is not replace:
-        if isinstance(mode, TorchFunctionMode):
-            help_text = (
-                'Use push_torch_function_mode instead.'
-            )
-        else:
-            help_text = (
-                'If you intended to completely override the preexisting mode, '
-                'pass ignore_preexisting=True.  This can result in unexpected '
-                'behavior; please consider rewriting your mode to be a subclass '
-                'of TorchFunctionMode to make it compositional!'
-            )
-        raise ValueError(
-            'Attempted to enable_torch_function_mode, but there is already an '
-            f'active mode {old}.  {help_text}'
-        )
-    # NB: we don't require TorchFunctionMode since this is intended to also
-    # let you directly pass a Tensor subclass type to "mode-ify" it.
-    if not hasattr(mode, '__torch_function__'):
-        raise ValueError(
-            'The argument passed to enable_torch_function_mode must implement '
-            '__torch_function__'
-        )
-    _set_torch_function_mode(mode)
-    try:
-        yield
-    finally:
-        _set_torch_function_mode(old)
+    return _enable_mode(mode, TorchFunctionModeInfo(), replace=replace, ignore_preexisting=ignore_preexisting)
 
 
 @contextlib.contextmanager
--- torch/utils/_python_dispatch.py
+++ torch/utils/_python_dispatch.py
@@ -2,33 +2,76 @@ import torch
 import contextlib
 from typing import Iterator
 
-# Context manager that causes all pytorch operators to dispatch to the passed-in
-# type's __torch_dispatch__ function.
-# operation that accepts no tensors but returns a tensor.
-#
-# enable_python_mode is affected by torch._C._DisableTorchDispatch.
-#
-# NB: Calling an operator inside __torch_dispatch__ does go through
-# __torch_dispatch__ again. Please use _DisableTorchDispatch inside
-# __torch_dispatch__ to prevent infinite recursion.
+from torch.utils._mode_utils import _enable_mode, _ModeInfo
+from torch._C import _get_python_mode, _set_python_mode
+from dataclasses import dataclass
+
+@dataclass
+class PythonModeInfo(_ModeInfo):
+    def __init__(self):
+        # hacky because torch_function mode and python_mode don't yet have parity
+        super().__init__(mode_name="python", mode_class=type(None),
+                         base_mode_class=type(None),
+                         required_fn="__torch_dispatch__")
+
+    def is_allowed_type(self, mode) -> bool:
+        return mode is None or isinstance(mode, type) and issubclass(mode, (torch.Tensor,))
+
+    def allowed_types_for_error_message(self) -> str:
+        return "Tensor-like class or None"
+
+    def get_mode(self):
+        return _get_python_mode()
+
+    def help_text(self, mode) -> str:
+        return (
+            'If you intended to completely override the preexisting mode, '
+            'pass ignore_preexisting=True.  This can result in unexpected '
+            'behavior; please consider rewriting your mode to be a subclass '
+            f'of {self.mode_class.__name__} to make it compositional!'
+        )
+
+    def set_mode(self, mode):
+        return _set_python_mode(mode)
+
 #
 # TODO: Limitations and things about enable_python_mode we should fix before exposing it:
-# - it currently cannot be nested. This should be simple to implement; we need a
-#   stack of TorchDispatchTypeObjects and the next bullet point.
-# - We need a better user-facing api for torch._C._DisableTorchDispatch that
+# - We need a better user-facing api for _DisableTorchDispatch that
 #   is able to selectively disable __torch_dispatch__ of a particular class.
 # - It doesn't work with the tensor constructors (torch.tensor, torch.Tensor)
 # - Better name (see https://github.com/pytorch/pytorch/pull/63496#discussion_r694091694)
 @contextlib.contextmanager
-def enable_python_mode(cls) -> Iterator[None]:
-    if cls.__torch_dispatch__ is torch.Tensor.__torch_dispatch__:
-        raise ValueError('The class passed to enable_python_mode '
-                         'must have a non-default __torch_dispatch__ classmethod')
-    if not isinstance(cls, type) or not issubclass(cls, (torch.Tensor,)):
-        raise ValueError('The argument passed to enable_python_mode '
-                         'must be the type of a Tensor subclass')
-    torch._C._enter_python_mode(cls)
-    try:
-        yield
-    finally:
-        torch._C._exit_python_mode()
+def enable_python_mode(mode, *, replace=None, ignore_preexisting=False) -> Iterator[None]:
+    """
+    Context manager that causes all pytorch operators to dispatch to the passed-in
+    type's __torch_dispatch__ function, including operations that accept no tensors
+    but return a tensor.
+
+    This function is non-compositional; if there is already an existing mode,
+    it will raise an error
+
+    This function is safe to use inside a ``__torch_dispatch__`` mode handler,
+    as the mode is guaranteed to be disabled in this context.  You can use
+    this context manager to reinstate the mode so that calls to overridable
+    APIs recursively call back into your mode handler (this can easily cause
+    infinite loops, so use with care!)
+
+    enable_python_mode is affected by _DisableTorchDispatch.
+
+    Args:
+        mode (:class:`PythonMode`, Tensor-like class or None): the
+            mode to set as current mode.  If you pass a Tensor-like class,
+            it will be treated as a non-compositional mode with no state,
+            which is convenient if you have an existing tensor subclass
+            that you'd like to apply globally in a quick and dirty way.
+            Passing None will disable the current mode.
+        replace (:class:`PythonMode` or Tensor-like class): the
+            mode to replace.  You can use this argument to change the mode in
+            a situation where you know what the current mode is (and you are
+            intentionally overwriting it.)  If you don't know what the current
+            mode is, use ``ignore_preexisting`` instead.
+        ignore_preexisting (bool): if True, ignore any preexisting mode
+            and overwrite it with the passed mode.
+    """
+
+    return _enable_mode(mode, mode_info=PythonModeInfo(), replace=replace, ignore_preexisting=ignore_preexisting)
