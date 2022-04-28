a01080375f5b02f991f3e47682e4d2bcf1a6f740
Jerry Zhang <jerryzh168@gmail.com>
2022-04-28 14:34:16 -0700

    [quant][fx][bc-breaking] Add required example_inputs argument to prepare_fx and prepare_qat_fx
    Summary:
    FX Graph Mode Quantization needs to know whether an fx node is a floating point Tensor before it can decide whether to
    insert observer/fake_quantize module or not, since we only insert observer/fake_quantize module for floating point Tensors.
    Currently we have some hacks to support this by defining some rules like `NON_OBSERVABLE_ARG_DICT` (https://github.com/pytorch/pytorch/blob/master/torch/ao/quantization/fx/utils.py#L496), but this approach is fragile and we do not plan to maintain it long term in the pytorch code base.
    
    As we discussed in the design review, we'd need to ask users to provide sample args and sample keyword args
    so that we can infer the type in a more robust way. This PR starts with changing the `prepare_fx` and `prepare_qat_fx` api to require user to either provide
    example arguments thrugh `example_inputs`, Note this api doesn't support kwargs, kwargs can make https://github.com/pytorch/pytorch/pull/76496#discussion_r861230047 simpler, but
    it will be rare, and even then we can still workaround with positional arguments, also `torch.jit.trace`(https://pytorch.org/docs/stable/generated/torch.jit.trace.html) and ShapeProp: https://github.com/pytorch/pytorch/blob/master/torch/fx/passes/shape_prop.py#L140 just have single positional args, we'll just use a single `example_inputs` argument for now.
    
    If needed, we can extend the api with an optional `example_kwargs`. e.g. in case when there are a lot of arguments for forward and it makes more sense to
    pass the arguments by keyword
    
    BC-breaking Note:
    Before:
    m = resnet18(...)
    m = prepare_fx(m, qconfig_dict)
    
    After:
    m = resnet18(...)
    m = prepare_fx(m, qconfig_dict, example_inputs=(torch.randn(1, 3, 224, 224),))
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    python test/test_quantization.py TestQuantizeFxModels
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 81e9c119d52f6db6994df875bf50d1eff9915729
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76496

--- torch/ao/quantization/quantize_fx.py
+++ torch/ao/quantization/quantize_fx.py
@@ -178,6 +178,7 @@ def _prepare_fx(
     model: torch.nn.Module,
     qconfig_dict: Any,
     is_qat: bool,
+    example_inputs: Tuple[Any],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     equalization_qconfig_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
@@ -247,6 +248,7 @@ forward graph of the parent module,
         qconfig_dict,
         is_qat,
         tracer.node_name_to_scope,
+        example_inputs=example_inputs,
         prepare_custom_config_dict=prepare_custom_config_dict,
         equalization_qconfig_dict=equalization_qconfig_dict,
         backend_config_dict=backend_config_dict,
@@ -262,6 +264,7 @@ def _prepare_standalone_module_fx(
     model: torch.nn.Module,
     qconfig_dict: Any,
     is_qat: bool,
+    example_inputs: Tuple[Any],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
 ) -> GraphModule:
@@ -291,6 +294,7 @@ def _prepare_standalone_module_fx(
         model,
         qconfig_dict,
         is_qat,
+        example_inputs,
         prepare_custom_config_dict,
         backend_config_dict=backend_config_dict,
         is_standalone_module=True,
@@ -340,6 +344,7 @@ def fuse_fx(
 def prepare_fx(
     model: torch.nn.Module,
     qconfig_dict: Any,
+    example_inputs: Tuple[Any],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     equalization_qconfig_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
@@ -450,6 +455,7 @@ def prepare_fx(
             "preserved_attributes": ["preserved_attr"],
           }
 
+      * `example_inputs`: (required) Example inputs for forward function of the model
       * `equalization_qconfig_dict`: equalization_qconfig_dict is a dictionary
         with a similar structure as qconfig_dict except it will contain
         configurations specific to equalization techniques such as input-weight
@@ -480,7 +486,8 @@ def prepare_fx(
                     model(image)
 
         qconfig_dict = {"": qconfig}
-        prepared_model = prepare_fx(float_model, qconfig_dict)
+        example_inputs = (torch.randn(1, 3, 224, 224),)
+        prepared_model = prepare_fx(float_model, qconfig_dict, example_inputs=example_inputs)
         # Run calibration
         calibrate(prepared_model, sample_inference_data)
 
@@ -490,6 +497,7 @@ def prepare_fx(
         model,
         qconfig_dict,
         False,  # is_qat
+        example_inputs,
         prepare_custom_config_dict,
         equalization_qconfig_dict,
         backend_config_dict,
@@ -499,6 +507,7 @@ def prepare_fx(
 def prepare_qat_fx(
     model: torch.nn.Module,
     qconfig_dict: Any,
+    example_inputs: Tuple[Any],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
 ) -> ObservedGraphModule:
@@ -507,6 +516,7 @@ def prepare_qat_fx(
     Args:
       * `model`: torch.nn.Module model, must be in train mode
       * `qconfig_dict`: see :func:`~torch.ao.quantization.prepare_fx`
+      * `example_inputs`: see :func:`~torch.ao.quantization.prepare_fx`
       * `prepare_custom_config_dict`: see :func:`~torch.ao.quantization.prepare_fx`
       * `backend_config_dict`: see :func:`~torch.ao.quantization.prepare_fx`
 
@@ -538,6 +548,7 @@ def prepare_qat_fx(
         model,
         qconfig_dict,
         True,  # is_qat
+        example_inputs,
         prepare_custom_config_dict,
         backend_config_dict=backend_config_dict,
     )
